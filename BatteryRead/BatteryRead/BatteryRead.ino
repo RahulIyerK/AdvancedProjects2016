b24924d738099dd62bd309898d1e31b49e1e320b
RahulIyerK <rahul.iyer2@gmail.com>
2016-11-11 00:18:15 -0800

    made changes to Battery Voltage Calculation

--- BatteryRead/BatteryRead/BatteryRead.ino
+++ BatteryRead/BatteryRead/BatteryRead.ino
@@ -1,13 +1,21 @@
-void setup() {
-  // put your setup code here, to run once:
-Serial.begin(9600);
+void setup() 
+{
+  Serial.begin(9600);
 }
 
-//const double VOLTAGE_ADC_RATIO = ((3.3 / 1023) / ((3.3 / (3.3 + 1)) * 4.2)) * 4.2;
+////const double VOLTAGE_ADC_RATIO = ((3.3 / 1023) / ((3.3 / (3.3 + 1)) * 4.2)) * 4.2;
+//
+//const double VOLTAGE_ADC_RATIO = (3.2232 / 1023) * ((3.3 + 1)/3.3);
 
-const double VOLTAGE_ADC_RATIO = (3.2232 / 1023) * ((3.3 + 1)/3.3);
+const double INPUT_MAX_VOLTS = 3.3;
+const double R1 = 1; //1k resistor
+const double R2 = 3.3; //3.3k resistor
 
-void loop() {
-  // put your main code here, to run repeatedly:
-Serial.println(analogRead(1) * VOLTAGE_ADC_RATIO);
+void loop() 
+{
+//Serial.println(analogRead(1) * VOLTAGE_ADC_RATIO);
+
+int voltage_DividerOut = analogRead(1);
+
+Serial.println(voltage_DividerOut * (INPUT_MAX_VOLTS / 1023) * ((R1 + R2)/ R2));
 }
