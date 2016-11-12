f3b6af2afb2d689895237a1fd70cf0567fdeb221
RahulIyerK <rahul.iyer2@gmail.com>
2016-11-11 22:05:52 -0800

    fixed issues with BatteryRead sketch (Vcc on Pro Mini was 3.496 volts (tested using a multimeter) instead of stated 3.3V)

--- BatteryRead/BatteryRead/BatteryRead.ino
+++ BatteryRead/BatteryRead/BatteryRead.ino
@@ -7,15 +7,16 @@ void setup()
 //
 //const double VOLTAGE_ADC_RATIO = (3.2232 / 1023) * ((3.3 + 1)/3.3);
 
-const double INPUT_MAX_VOLTS = 3.3;
-const double R1 = 1; //1k resistor
-const double R2 = 3.3; //3.3k resistor
+const double INPUT_MAX_VOLTS = 3.496;
+const double R1 = .99; //1k resistor
+const double R2 = 3.26; //3.3k resistor
 
 void loop() 
 {
-//Serial.println(analogRead(1) * VOLTAGE_ADC_RATIO);
 
 int voltage_DividerOut = analogRead(1);
 
+Serial.print(voltage_DividerOut);
+Serial.print(" , ");
 Serial.println(voltage_DividerOut * (INPUT_MAX_VOLTS / 1023) * ((R1 + R2)/ R2));
 }
