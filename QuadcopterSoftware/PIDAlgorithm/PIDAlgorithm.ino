d8d82bcad7e3eac54ca6b1fee5b4a74649dbd4a7
Aaron Wilhelm <aaron.j.wilhelm@gmail.com>
2017-05-16 19:02:13 -0700

    Updates to the PID Algorithm code

--- QuadcopterSoftware/PIDAlgorithm/PIDAlgorithm.ino
+++ QuadcopterSoftware/PIDAlgorithm/PIDAlgorithm.ino
@@ -3,6 +3,7 @@
 #define PITCH 0
 #define ROLL 1
 #define YAW 2
+#define FACTOR 10
 
 //don't make these constants as the controller will change them while tuning
 double pitchPK = 0.5;
@@ -20,21 +21,24 @@ double yawDK = 0.5;
 class PIDAlgorithm {
   public:
     PIDAlgorithm(double pConstant, double iConstant, double dConstant, int minMaxOfRange){
-      current = 0;
-      setpoint = 0;
+      PID_current = 0;
+      PID_setpoint = 0;
       error_sum = 0;
       pConst = pConstant;
       iConst = iConstant;
       dConst = dConstant;
       prev_error = 0;
       minMax = minMaxOfRange;
+      double pidMinMax = (double)PIDValueMinMax;//typecast to a double to avoid those dreadful overflow problems
+      double maxError = pidMinMax * 2;//get the min/max of pid
+      maxPIDError = (pConst * maxError) + (iConst * maxSize * maxError) + (dConst * maxError * 2); //calculate the total range that the error could be
     }
     
     //returns the correction from the PID algorithm
     //make sure that you update the current and setpoint values so that the algorithm
     //reflects the most up-to-date data
     double getCorrection(){
-      int error = setpoint - current;
+      int error = PID_setpoint - PID_current;
       if(pastErrors.count() > maxSize){ //if we are about to go over...
         error_sum -= pastErrors.pop(); //then pop the queue
       }
@@ -47,12 +51,12 @@ class PIDAlgorithm {
 
     //the setpoint passed in is a raw setpoint value
     void setSetpoint(int rawAmount){
-      setpoint = rawToPIDNum(rawAmount); //the setpoint stored is converted to the PID range
+      PID_setpoint = rawToPIDNum(rawAmount); //the setpoint stored is converted to the PID range
     }
 
     //the current raw value that the system is at
     void setCurrent(int rawAmount) {
-      current = rawToPIDNum(rawAmount); //the current value stored is converted to the PID range
+      PID_current = rawToPIDNum(rawAmount); //the current value stored is converted to the PID range
     }
 
     //resets the PID constants to the values passed in
@@ -63,21 +67,20 @@ class PIDAlgorithm {
     }
 
     int rawToPIDNum(double raw){
-      return map(raw, -minMax, minMax, -PIDValueMinMax, PIDValueMinMax);
+      //return map(raw, -minMax, minMax, -PIDValueMinMax, PIDValueMinMax);
+      return (int)(raw * FACTOR);
       //a PID number is the raw data (in the range [-minMax, minMax]) mapped to the range [-PIDValueMinMax, PIDValueMinMax]. 
     }
 
     //returns a correction ranging from -1.0 to 1.0
+    //PIDError is the error from the PID algorithm ("PID Value")
     double PIDErrorToCorrection(double PIDError){
-      double pidMinMax = (double)PIDValueMinMax;//typecast to a double to avoid those dreadful overflow problems
-      double maxError = pidMinMax * 2;//get the min/max of pid
-      double maxPIDError = maxError + (maxSize * maxError) + (maxError * 2); //calculate the total range that the error could be
       return (PIDError / maxPIDError);
     }
 
   private:
-    int PID_current; //the current value * 100 ("PID number")
-    int PID_setpoint; //the setpoint value * 100 ("PID number")
+    int PID_current; //the current value * FACTOR ("PID number")
+    int PID_setpoint; //the setpoint value * FACTOR ("PID number")
     
     int32_t error_sum; //the total error within maxSize cycles..32 bit for large sum ("PID number")
     
@@ -94,7 +97,9 @@ class PIDAlgorithm {
     int minMax; //the max/min of the possible raw inputs (raw values)
     
     //i.e. 30 if the possible inputs are from -30 to 30
-    const int PIDValueMinMax = minMax * 100;
+    const int PIDValueMinMax = minMax * FACTOR;
+
+    double maxPIDError;
 };
 
 PIDAlgorithm pitchAlg = PIDAlgorithm(pitchPK, pitchIK, pitchDK, 30);
